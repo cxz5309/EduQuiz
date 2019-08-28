a142eeaa80def5f69759aed8f53b17f7176245c1
cxz5309 <cxz5309@gmail.com>
2019-08-28 16:28:27 +0900

    카운터

--- Assets/02Scripts/Basic/EnemyInfo_B.cs
+++ Assets/02Scripts/Basic/EnemyInfo_B.cs
@@ -108,6 +108,12 @@ public class EnemyInfo_B : MonoBehaviour
         // 초당 MoveSpeed의 거리를 이동
     }
 
+    void FastMove()
+    {
+        ani.SetBool("walk", false);
+        ani.SetBool("run", true);
+    }
+
     // 적 공격 메소드
     IEnumerator Attack() {
         ani.SetBool("walk", false);
